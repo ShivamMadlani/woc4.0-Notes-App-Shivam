2f5510cec3046e0173b27f81b87cd30a6b42e4f7
Shivam <shivammadlani5@gmail.com>
2022-01-20 12:48:23 +0530

    added javascript

---
+++ main.js
@@ -0,0 +1,8 @@
+function add() {
+    var menu_2 = document.getElementById("menu_2");
+    menu_2.style.display = "inline-block";
+    const x = prompt('Enter name', 'Note');
+    if (x != null) {
+        document.getElementById("2").innerHTML = x;
+    }
+}
