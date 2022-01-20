efe157e45898ba8a4d931e9dc24bec5d1fefab74
Shivam <shivammadlani5@gmail.com>
2022-01-20 17:37:24 +0530

    .

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 function add() {
-    var menu_2 = document.getElementById("menu_2");
-    menu_2.style.display = "inline-block";
-    const x = prompt('Enter name', 'Note');
-    if (x != null) {
-        document.getElementById("2").innerHTML = x;
+    var id=2;
+    var name=['a','b','menu_2','menu_3','menu_4','menu_5'];
+    for(;id<=5;){
+        var m=document.getElementById("menu_2").id = name[3];
+        
+        console.log(m);
+
+        m.style.display = "inline-block";
+        const x = prompt('Enter name', 'Note');
+        if (x != null) {
+            document.getElementById(id).innerHTML = x;
+        }
+        id++;
+        break;  
     }
 }
 
