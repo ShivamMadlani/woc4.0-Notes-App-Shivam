af4291a98654202e065aa2e30f4d37e5caf52949
Shivam <shivammadlani5@gmail.com>
2022-01-24 19:30:30 +0530

    update

--- main.js
+++ main.js
@@ -1,45 +1,58 @@
-let index = 1;
-
-function add() {
-    var name = prompt("Enter the name of note", "Note");
-    if (name != null) {
-        const name_node = document.createTextNode(name);
-
-        const new_note = document.createElement("div");
-        const new_btns = document.createElement("div");
-        const h3 = document.createElement("h3");
-        const btn1 = document.createElement("button");
-        const btn2 = document.createElement("button");
-
-        h3.appendChild(name_node);
-        new_btns.appendChild(btn1);
-        new_btns.appendChild(btn2);
-
-        new_btns.className = "btns";
-        btn1.className = "addtsk";
-        btn2.className = "deltsk";
-        new_note.className = "menu";
-
-        btn1.innerText = "Add task";
-        btn2.innerText = "Del list";
-
-        new_note.style.top = "-60px";
-        new_btns.style.top = "210px";
-
-        new_note.appendChild(h3);
-        new_note.appendChild(new_btns);
-
-        document.getElementById("menu").insertAdjacentElement('afterend', new_note);
-
-        btn2.onclick = function () { del() };
-        index += 1;
+function new_refresh() {
+    const not_nme = prompt("Enter name of note", "Note");
+    if (not_nme != null) {
+        if (localStorage.getItem('notes') == null) {
+            notesarr = [];
+            notesarr.push(not_nme);
+            localStorage.setItem('notes', JSON.stringify(notesarr));
+        }
+        else {
+            notesarr_str = localStorage.getItem('notes');
+            notesarr = JSON.parse(notesarr_str);
+            notesarr.push(not_nme);
+            localStorage.setItem('notes', JSON.stringify(notesarr));
+        }
     }
+    refresh();
 }
 
-function del() {
-    if (confirm("Do you want to delete the last note?")) {
-        var mylist = document.getElementById("list");
-        mylist.removeChild(mylist.childNodes[index]);
-        index -= 1;
+function refresh() {
+    if (localStorage.getItem('notes') == null) {
+        notesarr = [];
+        localStorage.setItem('notes', JSON.stringify(notesarr));
     }
+    else {
+        notesarr_str = localStorage.getItem('notes');
+        notesarr = JSON.parse(notesarr_str);
+    }
+    let list = document.getElementById("list");
+    let str = "";
+    notesarr.forEach((element, index) => {
+        str += `
+    <li id="list">
+        <div class="menu" id="menu">
+            <h3 class="nme">${element}</h3>
+            <div class="btns">
+                <button class="addtsk">Add task</button>
+                <button class="deltsk" onclick="del(${index})">Del list</button>
+            </div>
+        </div>
+    </li>`;
+    });
+    list.innerHTML = str;
+}
+
+function add() {
+    new_refresh();
+    refresh();
+}
+
+
+function del(iindex) {
+    console.log("del");
+    notesarr_str = localStorage.getItem('notes');
+    notesarr = JSON.parse(notesarr_str);
+    notesarr.splice(iindex, 1);
+    localStorage.setItem('notes', JSON.stringify(notesarr));
+    refresh();
 }
