5588704bb6283c87700c960a41142ce5ba0a02fe
Shivam <shivammadlani5@gmail.com>
2022-01-25 15:57:02 +0530

    help!

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function refresh() {
         <div class="menu" id="menu">
             <h3 class="nme">${element}</h3>
             <div class="btns">
-                <button class="addtsk">Add task</button>
+                <button class="addtsk" onclick="tsk()">Add task</button>
                 <button class="deltsk" onclick="del(${index})">Del list</button>
             </div>
         </div>
@@ -55,4 +55,47 @@ function del(iindex) {
     notesarr.splice(iindex, 1);
     localStorage.setItem('notes', JSON.stringify(notesarr));
     refresh();
+}
+
+function add_tsk() {
+    console.log("adding tsk");
+    const name_tsk = prompt("Enter task");
+    if (name_tsk != null) {
+        if (localStorage.getItem('tasks') == null) {
+            taskarr = [];
+            taskarr.push(name_tsk);
+            localStorage.setItem('tasks', JSON.stringify(taskarr));
+        }
+        else {
+            taskarr_str = localStorage.getItem('notes');
+            taskarr = JSON.parse(taskarr_str);
+            taskarr.push(name_tsk);
+            localStorage.setItem('tasks', JSON.stringify(taskarr));
+        }
+    }
+    add_tsk2();
+}
+
+function add_tsk2() {
+    if (localStorage.getItem('tasks') == null) {
+        taskarr = [];
+        localStorage.setItem('tasks', JSON.stringify(taskarr));
+    }
+    else {
+        taskarr_str = localStorage.getItem('tasks');
+        taskarr = JSON.parse(notesarr_str);
+    }
+    let tsk_list = document.getElementById('taskss');
+    console.log(tsk_list);
+    let s = "";
+    taskarr.forEach(element => {
+        s += `<li>${element}<button id="edit"><i class="material-icons">edit</i></button> <button id="trash"><i
+        class="material-icons">delete</i></button></li>`;
+    });
+    tsk_list.innerHTML = s;
+}
+
+function tsk() {
+    add_tsk();
+    add_tsk2();
 }
